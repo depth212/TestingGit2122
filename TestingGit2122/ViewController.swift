a6ec48afe61423f0ef323c676ba01bf9cb55ea66
Michael Paulin <michael.paulin@icloud.com>
2020-09-30 09:49:47 -0500

    Initial Commit

---
+++ TestingGit2122/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TestingGit2122
+//
+//  Created by Paulin, Michael R on 9/30/20.
+//  Copyright © 2020 Paulin, Michael R. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
