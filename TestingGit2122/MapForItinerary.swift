7f025106f56a3ecf9f538ad525a0d3c5e3010c7f
Michael Paulin <michael.paulin@icloud.com>
2020-10-02 08:46:42 -0500

    Add new map-for-itinerary feature

---
+++ TestingGit2122/MapForItinerary.swift
@@ -0,0 +1,9 @@
+//
+//  MapForItinerary.swift
+//  TestingGit2122
+//
+//  Created by Paulin, Michael R on 9/30/20.
+//  Copyright © 2020 Paulin, Michael R. All rights reserved.
+//
+
+import Foundation
